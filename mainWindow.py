094bddf2ad648e9b66ce7d534cdcc64175c96393
Neil Wilcoxson <neil@neilwilcoxson.com>
2018-07-14 19:48:34 -0700

    GUI changed to grid

--- mainWindow.py
+++ mainWindow.py
@@ -6,6 +6,7 @@ LARGE_FONT = ("Arial Black", 12)
 
 class MainWindow(tk.Frame):
     users = {"John Doe": "John Doe", "Jane Smith": "Jane Smith", "Someother Guy": "Someother Guy"}
+    user_list = None
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -14,44 +15,45 @@ class MainWindow(tk.Frame):
     def draw(self):
         # title
         label = tk.Label(self, text="Can't See This", font=LARGE_FONT)
-        label.place(x=45, y=10, in_=self)
+        label.grid(row=0, columnspan=4)
 
         # user list
         scrollbar = tk.Scrollbar(self, orient="vertical")
-        user_list = tk.Listbox(self, height=10, width=32, yscrollcommand=scrollbar.set)
-        scrollbar.config(command=user_list.yview)
-        self._fill_user_list(user_list)
-        user_list.place(x=10, y=50, in_=self)
-        scrollbar.place(x=205, y=50, in_=self)
+
+        self.user_list = tk.Listbox(self, height=10, width=36, yscrollcommand=scrollbar.set, selectmode=tk.SINGLE)
+        self._fill_user_list()
+        self.user_list.grid(row=1, padx=(1, 0), columnspan=3)
+
+        scrollbar.config(command=self.user_list.yview)
+        scrollbar.grid(row=1, column=3, padx=(0, 10), sticky=tk.N + tk.S)
 
         # buttons
-        firstRow = 220
-        secondRow = 250
-
         add_user_button = tk.Button(self, text="Add User", command=self.add_user)
-        add_user_button.place(x=10, y=firstRow, in_=self)
+        add_user_button.grid(row=2, column=0, padx=(10, 1), pady=10)
 
         remove_user_button = tk.Button(self, text="Remove User", command=self.remove_user)
-        remove_user_button.place(x=75, y=firstRow, in_=self)
+        remove_user_button.grid(row=2, column=1, padx=1, pady=10)
 
         edit_user_button = tk.Button(self, text="Edit User", command=self.edit_user)
-        edit_user_button.place(x=160, y=firstRow, in_=self)
+        edit_user_button.grid(row=2, column=2, padx=1, pady=10)
 
         enable_button = tk.Button(self, text="Enable Protection", command=self.enable_protection)
-        enable_button.place(x=60, y=secondRow, in_=self)
+        enable_button.grid(row=3, column=1, padx=1, pady=10)
 
     def add_user(self):
         print("Add user pushed")
 
     def remove_user(self):
         print("Remove user button pushed")
+        self.user_list.delete(self.user_list.curselection())
 
     def edit_user(self):
         print("Edit user button pushed")
+        print("Trying to edit", self.user_list.get(self.user_list.curselection()))
 
     def enable_protection(self):
         print("Enable protection button pushed")
 
-    def _fill_user_list(self, user_list):
+    def _fill_user_list(self):
         for user in self.users.keys():
-            user_list.insert(0, user)
+            self.user_list.insert(0, user)
